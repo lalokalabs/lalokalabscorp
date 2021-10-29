e9fd61fa3e19a61a158696b94be4c08f89814423
Muhammad Fathur Rahman <fathur@xoxzo.com>
2021-10-29 11:41:39 +0800

    feat: update footer style

--- footer.php
+++ footer.php
@@ -24,7 +24,7 @@
 					'container_class' => 'flex flex-wrap flex-grow text-center lg:-mb-10 md:mt-0',
 					'menu_class'      => 'flex flex-wrap flex-grow text-center lg:-mb-10 md:mt-0',
 					'theme_location'  => 'footer',
-					'li_class'        => 'w-full px-4 mt-4 lg:w-1/4 md:w-1/4',
+					'li_class'        => 'w-full sm:w-auto px-4 mt-4',
 					'fallback_cb'     => false,
 				)
 			);
