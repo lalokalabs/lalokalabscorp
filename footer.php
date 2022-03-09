5248b515da87714e33efdda7b87776bdac39e3c4
hyejeongpark <phj1698@gmail.com>
2022-03-09 15:39:15 +0000

    Add youtube link to footer

--- footer.php
+++ footer.php
@@ -9,9 +9,9 @@
 <!-- simple footer -->
 
 <footer class="text-white bg-cool-gray-900">
-	<div class="container px-4 lg:px-14 mx-auto flex flex-col sm:flex-row items-center py-10">
+	<div class="container px-4 lg:px-14 mx-auto flex flex-col md:flex-row items-center py-10">
 		<!-- company info -->
-		<div class="w-72 border-cool-gray-600 border-t sm:border-t-0 sm:border-r sm:pr-10 order-last sm:order-first mt-8 sm:mt-0 pt-4 sm:pt-0">
+		<div class="w-72 border-cool-gray-600 border-t md:border-t-0 md:border-r md:pr-10 order-last md:order-first mt-8 md:mt-0 pt-4 md:pt-0">
 			<div class="text-center md:text-left">
 				<a class="flex items-center justify-center text-base font-medium title-font md:justify-start mt-4">
 					&copy; <?php echo date_i18n( 'Y' );?> LaLoka Labs OÜ
@@ -67,6 +67,12 @@
 						d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z">
 					</path>
 					<circle cx="4" cy="4" r="2" stroke="none"></circle>
+				</svg>
+			</a>
+			<!-- youtube -->
+			<a href="https://www.youtube.com/channel/UCbY6Fri8mTYi8XG63SEpWXA" class="cursor-pointer text-gray-300 hover:text-white">
+				<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="w-7 h-7" viewBox="0 0 16 16">
+				  <path d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.007 2.007 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.007 2.007 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31.4 31.4 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.007 2.007 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A99.788 99.788 0 0 1 7.858 2h.193zM6.4 5.209v4.818l4.157-2.408L6.4 5.209z"/>
 				</svg>
 			</a>
 		</div>
